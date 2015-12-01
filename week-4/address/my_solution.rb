8825607f899c80f3b9450f9a2b39bf7d678ccfe7
David Rothschild <david@rothschildsolutions.com>
2015-12-01 14:02:27 -0800

    add format address method

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,10 @@
+# Format an Address
+
+# I worked on this challenge on my lonesome.
+
+
+# Your Solution Below
+
+def make_address (street, city, state, zipcode)
+  return 'You live at ' + street + ', in the beautiful city of ' + city + ', ' + state + '. Your zip is ' + zipcode.to_s + '.'
+end
