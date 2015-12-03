9fd4ad4d7173326ab4ec80f463d51af03885ca5d
David Rothschild <david@rothschildsolutions.com>
2015-12-03 13:09:22 -0800

    concatenate two arrays

---
+++ week-4/concatenate-arrays/my_solution.rb
@@ -0,0 +1,14 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def array_concat(array_1, array_2)
+  # Your code here
+  for item in array_2
+    array_1.push item
+  end
+  return array_1
+end
