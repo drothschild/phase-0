e8a02d1ed5425abdfb336a0944857d94b9147f73
David Rothschild <david@rothschildsolutions.com>
2015-12-02 17:14:30 -0800

    solve flow control problems

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,13 @@
+# Simple Substrings
+
+# I worked on this challenge  myself.
+
+
+# Your Solution Below
+def welcome(address)
+  if address =~ /CA/
+    return "Welcome to California"
+  else
+    return "You should move to California"
+  end
+end
