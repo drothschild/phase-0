fb75094dca938a7322e7fb23ec12d270e0d469b4
David Rothschild <david@rothschildsolutions.com>
2015-12-08 13:10:26 -0800

    add some pseudocode to account-groups

---
+++ week-5/acct_groups.rb
@@ -0,0 +1,22 @@
+#
+
+def make_acct_groups (names)
+  #input: arr of names
+  #output: array of array of names
+  #pseudo-code:
+  # Create acct_groups, an empty array, with the default value
+  # shuffle names up
+  # set i to 0
+  # LOOP through names
+    #  if acct_groups (group atindex i) is greater than 4 or ( group at index i >2 and only 3 names remain)
+      # make a new array in acct_groups
+      #i = i +1
+    #  end if
+    # add name to acct_group (group_at_index_i)
+  # END LOOP
+
+  #Return acct_groups
+end
+
+# puts make_acct_groups ("a","b","c","d")
+# make_acct_groups(12 names)
