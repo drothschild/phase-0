15dfd5620514b238f3caba4d056ca11b3fea4484
David Rothschild <david@rothschildsolutions.com>
2015-12-08 18:09:53 -0800

    add reflection questions

--- week-5/gps2_2.rb
+++ week-5/gps2_2.rb
@@ -145,3 +145,11 @@ add_item("apple", 1)
 display_list
 # => {"Apples" => 1}
 
+
+
+#What did you learn about pseudocode from working on this challenge?
+#What are the tradeoffs of using Arrays and Hashes for this challenge?
+#What does a method return?
+#What kind of things can you pass into methods as arguments?
+#How can you pass information between methods?
+#What concepts were solidified in this challenge, and what concepts are still confusing?
