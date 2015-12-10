030f0c71ed1576d12bdd522ce00ff1d1bbc41f11
David Rothschild <david@rothschildsolutions.com>
2015-12-09 16:36:02 -0800

    Add more descriptive ArgumentError.

--- week-5/die-class/my_solution.rb
+++ week-5/die-class/my_solution.rb
@@ -16,7 +16,7 @@
 class Die
   def initialize(sides)
     if sides <1
-      raise ArgumentError
+      raise ArgumentError.new ("Dice need at least 1 side.")
     end
     @sides = sides
   end
@@ -41,3 +41,10 @@ end
 
 
 # 4. Reflection
+
+#What is an ArgumentError and why would you use one?
+#What new Ruby methods did you implement? What challenges and successes did you have in implementing them?
+#What is a Ruby class?
+#Why would you use a Ruby class?
+#What is the difference between a local variable and an instance variable?
+#Where can an instance variable be used?
